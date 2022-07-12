c79aeee09889975a812d38c7bf1b426e1b263b64
jonathanprozzi <jonathanprozzi@gmail.com>
2022-07-12 10:17:55 -0400

    initial project scaffolding via nx

---
+++ apps/dungeon-master-v2/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'dungeon-master-v2',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/dungeon-master-v2',
+};
