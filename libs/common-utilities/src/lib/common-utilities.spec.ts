d107574166097ac4a50316b3e466b8622afebbcc
jonathanprozzi <jonathanprozzi@gmail.com>
2022-07-12 10:21:03 -0400

    added common-utilities to libs with import path @dmv2

---
+++ libs/common-utilities/src/lib/common-utilities.spec.ts
@@ -0,0 +1,7 @@
+import { commonUtilities } from './common-utilities';
+
+describe('commonUtilities', () => {
+  it('should work', () => {
+    expect(commonUtilities()).toEqual('common-utilities');
+  });
+});
---
+++ libs/common-utilities/src/lib/common-utilities.ts
@@ -0,0 +1,3 @@
+export function commonUtilities(): string {
+  return 'common-utilities';
+}
