ed0cf378fae45997f3c72ed4d3311352b493d3aa
Konrad Gnat <konradmgnat@gmail.com>
2022-11-06 23:24:34 -0600

    update application migrate scripts

--- services/dmv2-hasura/migrations/default/1653448061860_create_table_public_applications/up.sql
+++ services/dmv2-hasura/migrations/default/1653448061860_create_table_public_applications/up.sql
@@ -23,7 +23,7 @@ CREATE TABLE "public"."applications" (
   "pledge_readiness" boolean DEFAULT false,
   "referred_by" uuid,
   PRIMARY KEY ("id"),
-  FOREIGN KEY ("skill_type") REFERENCES "public"."skill_types"("skill_type") ON UPDATE restrict ON DELETE restrict,
+  FOREIGN KEY ("skill_type") REFERENCES "public"."technical_skill_types"("skill_type") ON UPDATE restrict ON DELETE restrict,
   FOREIGN KEY ("dao_familiarity") REFERENCES "public"."dao_familiarities"("dao_familiarity") ON UPDATE restrict ON DELETE restrict,
   FOREIGN KEY ("availability") REFERENCES "public"."cohort_availabilities"("cohort_availability") ON UPDATE restrict ON DELETE restrict,
   FOREIGN KEY ("referred_by") REFERENCES "public"."members"("id") ON UPDATE restrict ON DELETE restrict,
