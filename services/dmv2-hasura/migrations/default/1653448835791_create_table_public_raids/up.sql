d4b00b1cd1e7ce283375a75c958b7cefb16f0d3e
Konrad Gnat <konradmgnat@gmail.com>
2022-11-15 06:38:03 +0900

    migrations

--- services/dmv2-hasura/migrations/default/1653448835791_create_table_public_raids/up.sql
+++ services/dmv2-hasura/migrations/default/1653448835791_create_table_public_raids/up.sql
@@ -1,2 +1,25 @@
-CREATE TABLE "public"."raids" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "name" text NOT NULL, "status" text NOT NULL, "category" text NOT NULL, "cleric" uuid, "invoice_address" text, "start_date" timestamptz, "end_date" timestamptz, "consultation" uuid NOT NULL, "portfolio" uuid, "airtable_id" text, "escrow_index" integer, "locker_hash" text, PRIMARY KEY ("id") , FOREIGN KEY ("status") REFERENCES "public"."raid_statuses"("raid_status") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("category") REFERENCES "public"."raid_categories"("raid_category") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("cleric") REFERENCES "public"."members"("id") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("consultation") REFERENCES "public"."consultations"("id") ON UPDATE restrict ON DELETE restrict, UNIQUE ("id"));
+CREATE TABLE "public"."raids" (
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "v1_id" text,
+  "name" text NOT NULL,
+  "status" text NOT NULL,
+  "category" text NOT NULL,
+  "cleric" uuid,
+  "invoice_address" text,
+  "start_date" timestamptz,
+  "end_date" timestamptz,
+  "consultation" uuid NOT NULL,
+  "portfolio" uuid,
+  "airtable_id" text,
+  "escrow_index" integer,
+  "locker_hash" text,
+  "created_at" date DEFAULT NOW(),
+  "updated_at" date DEFAULT NOW(),
+  PRIMARY KEY ("id") ,
+  FOREIGN KEY ("status") REFERENCES "public"."raid_statuses"("raid_status") ON UPDATE restrict ON DELETE restrict,
+  FOREIGN KEY ("category") REFERENCES "public"."raid_categories"("raid_category") ON UPDATE restrict ON DELETE restrict,
+  FOREIGN KEY ("cleric") REFERENCES "public"."members"("id") ON UPDATE restrict ON DELETE restrict,
+  FOREIGN KEY ("consultation") REFERENCES "public"."consultations"("id") ON UPDATE restrict ON DELETE restrict,
+  UNIQUE ("id")
+);
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
--- services/dmv2-hasura/migrations/default/1653449542538_create_table_public_raid_roles_required/up.sql
+++ services/dmv2-hasura/migrations/default/1653449542538_create_table_public_raid_roles_required/up.sql
@@ -1,2 +1,10 @@
-CREATE TABLE "public"."raid_roles_required" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "raid" uuid NOT NULL, "role" text NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("raid") REFERENCES "public"."raids"("id") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("role") REFERENCES "public"."guild_classes"("guild_class") ON UPDATE restrict ON DELETE restrict, UNIQUE ("id"));
+CREATE TABLE "public"."raid_roles_required" (
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "raid" uuid NOT NULL,
+  "role" text NOT NULL,
+  PRIMARY KEY ("id") ,
+  FOREIGN KEY ("raid") REFERENCES "public"."raids"("id") ON UPDATE restrict ON DELETE restrict,
+  FOREIGN KEY ("role") REFERENCES "public"."guild_classes"("guild_class") ON UPDATE restrict ON DELETE restrict,
+  UNIQUE ("id")
+);
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
