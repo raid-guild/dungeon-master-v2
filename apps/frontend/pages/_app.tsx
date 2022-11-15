118ceb8826632652bd56e63769888a81f9c76e08
jonathanprozzi <jonathanprozzi@gmail.com>
2022-11-15 14:55:59 -0500

    added NextSeo, Design System, basic app structure, and public images

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,18 +1,39 @@
+import * as React from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import { DefaultSeo } from 'next-seo';
+import { RGThemeProvider } from '@raidguild/design-system';
+import SiteLayout from '../components/SiteLayout';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <Head>
-        <title>Welcome to dungeon-master-v2!</title>
-      </Head>
-      <main className="app">
+    <RGThemeProvider>
+      <DefaultSeo
+        titleTemplate="%s | Dungeon Master v1"
+        title="Dungeon Master"
+        defaultTitle="Raid Guild | Dungeon Master v1.5"
+        description="Adventurers, come and gather around the campfire."
+        canonical="https://site-url"
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://site-url',
+          site_name: 'Site title',
+          title: "Raid Guild | Dungeon Master v1.5'",
+          description: 'Adventurers, come and gather around the campfire.',
+          images: [
+            {
+              url: '/raidguild-logo', // replcace with your OG image
+              width: 1200,
+              height: 630,
+              alt: "Raid Guild logo: Crossed swords with 'Raid Guild'",
+            },
+          ],
+        }}
+      />
+      <SiteLayout>
         <Component {...pageProps} />
-      </main>
-    </>
+      </SiteLayout>
+    </RGThemeProvider>
   );
-}
-
-export default CustomApp;
+};
+export default MyApp;
