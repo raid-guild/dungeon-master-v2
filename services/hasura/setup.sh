b78d02f5c680eab2b483376da3b47c45376d0f4d
Matthew O'Connell <totallyterrific@tutanota.com>
2022-12-21 17:58:25 +0100

    chore(hasura): add docker auto-setup
    - switch to auto metadata/migration image
    - add setup container to auto-seed

---
+++ services/hasura/setup.sh
@@ -0,0 +1,5 @@
+apk update
+apk upgrade
+apk add curl
+/bin/sh -c "$(curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh)" -y -f
+timeout 5m /bin/sh -c "until hasura seed apply --database-name ${POSTGRES_DATABASE}; do sleep 10; done"
