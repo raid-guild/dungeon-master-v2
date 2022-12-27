2f3af310af897193adb166705abba1cfd7a32d38
ECWireless <elliott@coopallc.com>
2022-12-27 13:09:24 -0500

    Add TRANSACTIONS_QUERY

---
+++ apps/frontend/gql/queries/transactions.ts
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const TRANSACTIONS_QUERY = gql`
+  query MolochBalances($molochAddress: Bytes!, $first: Int, $skip: Int) {
+    balances(
+      first: $first,
+      skip: 10,
+      where: { molochAddress: $molochAddress, action_not: "summon" }
+      orderBy: timestamp
+      orderDirection: asc
+    ) {
+      id
+      timestamp
+      balance
+      tokenSymbol
+      tokenAddress
+      transactionHash
+      tokenDecimals
+      action
+      payment
+      tribute
+      amount
+      counterpartyAddress
+      proposalDetail {
+        proposalId
+        applicant
+        details
+        sharesRequested
+        lootRequested
+      }
+    }
+  }
+`;
