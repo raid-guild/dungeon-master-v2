88940ef99ff5319327ab5528f98f173265b0af3f
scottrepreneur <me@stwoh.com>
2023-11-10 13:36:29 -0600

    update footer links

--- apps/frontend/components/Footer.tsx
+++ apps/frontend/components/Footer.tsx
@@ -7,20 +7,31 @@ import {
   Stack,
   useMediaQuery,
 } from '@raidguild/design-system';
+import { useSession } from 'next-auth/react';
 
 const links = [
   // { label: 'Consultation Queue', link: 'https://www.raidguild.org/hire/1' },
   { label: 'Handbook', link: 'https://handbook.raidguild.org' },
   { label: 'Smart Invoice', link: 'https://smartinvoice.xyz' },
+  { label: 'Origins', link: 'https://origins.raidguild.org' },
+  {
+    label: 'Valhalla',
+    link: 'https://the-valhalla.vercel.app',
+    type: 'member',
+  },
 ];
 
 const Footer = () => {
   const [upTo780] = useMediaQuery('(max-width: 780px)');
+  const { data: session } = useSession();
+
+  const filteredLinks = !session ? _.reject(links, ['type', 'member']) : links;
+
   return (
     <Flex justify='space-around' mb='50px' mt='200px'>
       {!upTo780 ? <BuiltByRaidGuild /> : <Heading size='sm'>RaidGuild</Heading>}
       <Stack>
-        {_.map(links, (link) => (
+        {_.map(filteredLinks, (link) => (
           <Link href={link.link} isExternal key={link.link}>
             {link.label}
           </Link>
