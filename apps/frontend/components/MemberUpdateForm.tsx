2e736598fc31992389c8f805554d67b196fdd7a6
memosys <82053242+memosys@users.noreply.github.com>
2024-01-02 12:55:47 +0530

    Update Member Update Form Submit button

--- apps/frontend/components/MemberUpdateForm.tsx
+++ apps/frontend/components/MemberUpdateForm.tsx
@@ -237,18 +237,7 @@ const UpdateMemberForm = ({
                   localForm={localForm}
                 />
 
-                <Button
-                  isLoading={isSubmitting || sending}
-                  type='submit'
-                  width='full'
-                  color='raid'
-                  borderColor='raid'
-                  border='1px solid'
-                  size='md'
-                  textTransform='uppercase'
-                  fontSize='sm'
-                  fontWeight='bold'
-                >
+                <Button isLoading={isSubmitting || sending} type='submit'>
                   Update Profile
                 </Button>
               </Stack>
