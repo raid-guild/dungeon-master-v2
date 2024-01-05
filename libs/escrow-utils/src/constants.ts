9b4d0c8b8e4e8f706ce632c14b1633a73930e687
scottrepreneur <me@stwoh.com>
2024-01-04 21:46:29 -0600

    update zap address (#101)

--- libs/escrow-utils/src/constants.ts
+++ libs/escrow-utils/src/constants.ts
@@ -54,6 +54,7 @@ interface NetworkConfig {
   RESOLVERS: Resolvers;
   ZAP_ADDRESS?: Hex;
   DAO_ADDRESS?: Hex;
+  SPOILS_MANAGER?: Hex;
 }
 
 export const NETWORK_CONFIG: { [key: number]: NetworkConfig } = {
@@ -65,8 +66,11 @@ export const NETWORK_CONFIG: { [key: number]: NetworkConfig } = {
     WRAPPED_NATIVE_TOKEN: _.toLower(
       '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d'
     ) as Hex,
-    ZAP_ADDRESS: _.toLower('0x3f2da49623D40DEE50963055EEA0eFaB27e7b840') as Hex,
+    ZAP_ADDRESS: _.toLower('0xE52fFb810cFb882A5C1085b9c01F189cD356a9B4') as Hex,
     DAO_ADDRESS: _.toLower('0xf02fd4286917270cb94fbc13a0f4e1ed76f7e986') as Hex,
+    SPOILS_MANAGER: _.toLower(
+      '0x8C9c85b41a9491388320eA27d74A037D93d07C0F'
+    ) as Hex,
     TOKENS: {
       WXDAI: {
         decimals: 18,
