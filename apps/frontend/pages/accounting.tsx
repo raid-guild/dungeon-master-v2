a6308021ccc0cca91aa5d35fb129e1da4e9eadb6
growindiedev <rabhishekranjan@gmail.com>
2024-05-17 08:57:21 +0530

    cleaned up useAccountingV3

--- apps/frontend/pages/accounting.tsx
+++ apps/frontend/pages/accounting.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable simple-import-sort/imports */
 import {
   Button,
   Flex,
--- libs/dm-hooks/src/useAccountingV3.ts
+++ libs/dm-hooks/src/useAccountingV3.ts
@@ -1,53 +1,26 @@
-import { da } from 'date-fns/locale';
-import { useState, useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
 import { getAddress } from 'viem';
 
+const API_URL = 'https://safe-transaction-gnosis-chain.safe.global/api/v1';
 
-export const transformTokenBalances = (
-  tokenBalanceRes: any,
-  safeAddress: string
-): any => {
+const transformTokenBalances = (tokenBalanceRes, safeAddress: string) => {
   const fiatTotal = tokenBalanceRes.reduce(
-    (sum: number, balance: any): number => {
-      sum += Number(balance.fiatBalance);
-      return sum;
-    },
+    (sum: number, balance) => sum + Number(balance.fiatBalance),
     0
   );
-
   return { safeAddress, tokenBalances: tokenBalanceRes, fiatTotal };
 };
 
-export const listTokenBalances = async ({
-  safeAddress,
-}: {
-  safeAddress: string;
-}): Promise<any> => {
-  const url = 'https://safe-transaction-gnosis-chain.safe.global/api/v1'
-
-  if (!url) {
-    return {
-      error: 'Error fetching token balances. Please try again.'
-    };
-  }
-
+const listTokenBalances = async (safeAddress: string) => {
   try {
-    const res = await fetch(
-      `${url}/safes/${safeAddress}/balances/usd/`
-    );
+    const res = await fetch(`${API_URL}/safes/${safeAddress}/balances/usd/`);
     const data = await res.json();
-    console.log(data)
-    //
     return { data: transformTokenBalances(data, safeAddress) };
   } catch (err) {
-    return {
-      error: 'Error fetching token balances. Please try again.'
-    };
+    return { error: 'Error fetching token balances. Please try again.' };
   }
 };
 
-// this is a dummy hook
 const useAccountingV3 = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -56,27 +29,21 @@ const useAccountingV3 = () => {
   useEffect(() => {
     const fetchData = async () => {
       const checksum = getAddress('0x181ebdb03cb4b54f4020622f1b0eacd67a8c63ac');
-      console.log(checksum)
-      const { data, error } = await listTokenBalances({
-        safeAddress: checksum,
-      });
+      const response = await listTokenBalances(checksum);
 
-      if (error) {
-        setError(error);
-        console.log('error', error)
+      if (response.error) {
+        setError(response.error);
       } else {
-        setData(data);
-        console.log('data', data);
+        setData(response.data);
       }
 
       setLoading(false);
     };
 
     fetchData();
-    console.log('fetching data', data);
-  }, [])
+  }, []);
 
-  return { data: null, loading: false, error: null };
+  return { data, loading, error };
 };
 
 export default useAccountingV3;
