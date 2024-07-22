9822d58dfbe38aebe5d8c2da464cfb6c5f2369dd
growindiedev <rabhishekranjan@gmail.com>
2024-07-22 14:55:14 +0530

    cleanup accounting.tsx

--- apps/frontend/pages/accounting.tsx
+++ apps/frontend/pages/accounting.tsx
@@ -15,12 +15,12 @@ import {
   useAccountingV3,
 } from '@raidguild/dm-hooks';
 import { exportToCsv } from '@raidguild/dm-utils';
-import _ from 'lodash';
 import { useSession } from 'next-auth/react';
 import { NextSeo } from 'next-seo';
 import Papa from 'papaparse';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 
+import _ from 'lodash';
 import BalancesTable from '../components/BalancesTable';
 import SiteLayout from '../components/SiteLayout';
 import SpoilsTable from '../components/SpoilsTable';
@@ -28,16 +28,8 @@ import TransactionsTable from '../components/TransactionsTable';
 
 export const Accounting = () => {
   const { data: session } = useSession();
-  const [isV3, setIsV3] = useState(true);
 
   const token = _.get(session, 'token');
-  // const {
-  //   data: dataFromMolochV2,
-  //   loading,
-  //   error,
-  // } = useAccountingV2({
-  //   token,
-  // });
 
   const { data: memberData } = useMemberList({
     token,
@@ -58,90 +50,77 @@ export const Accounting = () => {
     (type: 'transactions' | 'balances' | 'spoils') => {
       let csvString = '';
       if (type === 'transactions') {
-        const formattedTransactions = transactionsWithPricesV3
-          // isV3 ? transactionsWithPricesV3 : transactionsWithPricesV2
-          .map((t) => ({
-            Date: t.date,
-            'Tx Explorer Link': t.txExplorerLink,
-            'Elapsed Days': t.elapsedDays,
-            Type: t.type,
-            Applicant: t.proposalApplicant,
-            'Applicant Member':
-              members[t.proposalApplicant.toLowerCase()]?.name || '-',
-            Shares: t.proposalShares,
-            Loot: t.proposalLoot,
-            Title: t.proposalTitle,
-            Counterparty: t.counterparty,
-            'Counterparty Member':
-              members[t.counterparty.toLowerCase()]?.name || '-',
-            'Token Symbol': t.tokenSymbol,
-            'Token Decimals': t.tokenDecimals,
-            'Token Address': t.tokenAddress,
-            Inflow: t.in,
-            'Inflow USD': t.priceConversion
-              ? `$${(t.in * t.priceConversion).toLocaleString()}`
-              : '$-',
-            Outflow: t.out,
-            'Outflow USD': t.priceConversion
-              ? `$${(t.out * t.priceConversion).toLocaleString()}`
-              : '$-',
-            Balance: t.balance,
-            'Balance USD': t.priceConversion
-              ? `$${(t.balance * t.priceConversion).toLocaleString()}`
-              : '$-',
-          }));
+        const formattedTransactions = transactionsWithPricesV3.map((t) => ({
+          Date: t.date,
+          'Tx Explorer Link': t.txExplorerLink,
+          'Elapsed Days': t.elapsedDays,
+          Type: t.type,
+          Applicant: t.proposalApplicant,
+          'Applicant Member':
+            members[t.proposalApplicant.toLowerCase()]?.name || '-',
+          Shares: t.proposalShares,
+          Loot: t.proposalLoot,
+          Title: t.proposalTitle,
+          Counterparty: t.counterparty,
+          'Counterparty Member':
+            members[t.counterparty.toLowerCase()]?.name || '-',
+          'Token Symbol': t.tokenSymbol,
+          'Token Decimals': t.tokenDecimals,
+          'Token Address': t.tokenAddress,
+          Inflow: t.in,
+          'Inflow USD': t.priceConversion
+            ? `$${(t.in * t.priceConversion).toLocaleString()}`
+            : '$-',
+          Outflow: t.out,
+          'Outflow USD': t.priceConversion
+            ? `$${(t.out * t.priceConversion).toLocaleString()}`
+            : '$-',
+          Balance: t.balance,
+          'Balance USD': t.priceConversion
+            ? `$${(t.balance * t.priceConversion).toLocaleString()}`
+            : '$-',
+        }));
         csvString = Papa.unparse(formattedTransactions);
       } else if (type === 'balances') {
         if (type === 'balances') {
-          const formattedBalances = balancesWithPricesV3
-            // isV3 ? balancesWithPricesV3 : balancesWithPricesV2
-            .map((b) => ({
-              Token: b.tokenSymbol,
-              'Tx Explorer Link': b.tokenExplorerLink,
-              Inflow: b.inflow.tokenValue,
-              'Inflow USD': b.priceConversion
-                ? `$${(
-                    Number(b.inflow.tokenValue) * b.priceConversion
-                  ).toLocaleString()}`
-                : '$-',
-              Outflow: b.outflow.tokenValue,
-              'Outflow USD': b.priceConversion
-                ? `$${(
-                    Number(b.outflow.tokenValue) * b.priceConversion
-                  ).toLocaleString()}`
-                : '$-',
-              Balance: b.closing.tokenValue,
-              'Balance USD': b.priceConversion
-                ? `$${(
-                    Number(b.closing.tokenValue) * b.priceConversion
-                  ).toLocaleString()}`
-                : '$-',
-            }));
+          const formattedBalances = balancesWithPricesV3.map((b) => ({
+            Token: b.tokenSymbol,
+            'Tx Explorer Link': b.tokenExplorerLink,
+            Inflow: b.inflow.tokenValue,
+            'Inflow USD': b.priceConversion
+              ? `$${(
+                  Number(b.inflow.tokenValue) * b.priceConversion
+                ).toLocaleString()}`
+              : '$-',
+            Outflow: b.outflow.tokenValue,
+            'Outflow USD': b.priceConversion
+              ? `$${(
+                  Number(b.outflow.tokenValue) * b.priceConversion
+                ).toLocaleString()}`
+              : '$-',
+            Balance: b.closing.tokenValue,
+            'Balance USD': b.priceConversion
+              ? `$${(
+                  Number(b.closing.tokenValue) * b.priceConversion
+                ).toLocaleString()}`
+              : '$-',
+          }));
           csvString = Papa.unparse(formattedBalances);
         }
+      } else if (type === 'spoils') {
+        const formattedSpoils = formattedSpoilsV3.map((s) => ({
+          Date: s.date,
+          Raid: s.raidName,
+          // TODO: Get this dynamically from the subgraph
+          'Token Symbol': 'wxDAI',
+          'To DAO Treasury': `$${s.parentShare.toLocaleString()}`,
+          'To Raid Party': `$${s.childShare.toLocaleString()}`,
+        }));
+        csvString = Papa.unparse(formattedSpoils);
       }
-      // else if (type === 'spoils') {
-      //   const formattedSpoils = spoils.map((s) => ({
-      //     Date: s.date,
-      //     Raid: s.raidName,
-      //     // TODO: Get this dynamically from the subgraph
-      //     'Token Symbol': 'wxDAI',
-      //     'To DAO Treasury': `$${s.parentShare.toLocaleString()}`,
-      //     'To Raid Party': `$${s.childShare.toLocaleString()}`,
-      //   }));
-      //   csvString = Papa.unparse(formattedSpoils);
-      // }
       exportToCsv(csvString, `raidguild-treasury-${type}`);
     },
-    [
-      // balancesWithPricesV2,
-      balancesWithPricesV3,
-      // isV3,
-      members,
-      // spoils,
-      // transactionsWithPricesV2,
-      transactionsWithPricesV3,
-    ]
+    [balancesWithPricesV3, members, formattedSpoilsV3, transactionsWithPricesV3]
   );
 
   return (
@@ -175,133 +154,52 @@ export const Accounting = () => {
 
           <TabPanels>
             <TabPanel>
-              <Tabs
-                align='start'
-                colorScheme='whiteAlpha'
-                variant='unstyled'
-                defaultIndex={0}
-                onChange={(index) => {
-                  if (index === 0) setIsV3(true);
-                  else setIsV3(false);
-                }}
+              <Flex
+                alignItems='right'
+                justifyContent='right'
+                marginBlock='20px'
               >
-                <Flex
-                  alignItems='right'
-                  justifyContent='space-between'
-                  marginBlock='20px'
+                <Button
+                  onClick={() => onExportCsv('balances')}
+                  size='sm'
+                  fontWeight='normal'
                 >
-                  <TabList>
-                    <Tab>
-                      <Heading size='sm'>V3 (current)</Heading>
-                    </Tab>
-                    <Tab>
-                      <Heading size='sm'>V2</Heading>
-                    </Tab>
-                  </TabList>
-                  <Button
-                    onClick={() => onExportCsv('balances')}
-                    size='sm'
-                    fontWeight='normal'
-                  >
-                    Export Balances
-                  </Button>
-                </Flex>
-
-                <TabPanels>
-                  <TabPanel>
-                    <BalancesTable data={balancesWithPricesV3} />
-                  </TabPanel>
-                  <TabPanel>
-                    {/* <BalancesTable data={balancesWithPricesV2} /> */}
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
+                  Export Balances
+                </Button>
+              </Flex>
+              <BalancesTable data={balancesWithPricesV3} />
             </TabPanel>
             <TabPanel>
-              <Tabs
-                align='start'
-                colorScheme='whiteAlpha'
-                variant='unstyled'
-                defaultIndex={0}
-                onChange={(index) => {
-                  if (index === 0) setIsV3(true);
-                  else setIsV3(false);
-                }}
+              <Flex
+                alignItems='right'
+                justifyContent='right'
+                marginBlock='20px'
               >
-                <Flex
-                  alignItems='right'
-                  justifyContent='space-between'
-                  marginBlock='20px'
+                <Button
+                  onClick={() => onExportCsv('transactions')}
+                  size='sm'
+                  fontWeight='normal'
                 >
-                  <TabList>
-                    <Tab>
-                      <Heading size='sm'>V3 (current)</Heading>
-                    </Tab>
-                    <Tab>
-                      <Heading size='sm'>V2</Heading>
-                    </Tab>
-                  </TabList>
-                  <Button
-                    onClick={() => onExportCsv('transactions')}
-                    size='sm'
-                    fontWeight='normal'
-                  >
-                    Export Transactions
-                  </Button>
-                </Flex>
-
-                <TabPanels>
-                  <TabPanel>
-                    <TransactionsTable
-                      data={transactionsWithPricesAndMembersV3}
-                    />
-                  </TabPanel>
-                  <TabPanel>
-                    {/* <TransactionsTable
-                      data={transactionsWithPricesAndMembersV2}
-                    /> */}
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
+                  Export Transactions
+                </Button>
+              </Flex>
+              <TransactionsTable data={transactionsWithPricesV3} />
             </TabPanel>
             <TabPanel>
-              <Tabs
-                align='start'
-                colorScheme='whiteAlpha'
-                variant='unstyled'
-                defaultIndex={0}
+              <Flex
+                alignItems='right'
+                justifyContent='right'
+                marginBlock='20px'
               >
-                <Flex
-                  alignItems='right'
-                  justifyContent='space-between'
-                  marginBlock='20px'
+                <Button
+                  onClick={() => onExportCsv('spoils')}
+                  size='sm'
+                  fontWeight='normal'
                 >
-                  <TabList>
-                    <Tab>
-                      <Heading size='sm'>V3 (current)</Heading>
-                    </Tab>
-                    <Tab>
-                      <Heading size='sm'>V2</Heading>
-                    </Tab>
-                  </TabList>
-                  <Button
-                    onClick={() => onExportCsv('spoils')}
-                    size='sm'
-                    fontWeight='normal'
-                  >
-                    Export Spoils
-                  </Button>
-                </Flex>
-
-                <TabPanels>
-                  <TabPanel>
-                    <SpoilsTable data={formattedSpoilsV3} />
-                  </TabPanel>
-                  <TabPanel>
-                    <div>This is the placeholder for v3 spoils data.</div>
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
+                  Export Spoils
+                </Button>
+              </Flex>
+              <SpoilsTable data={formattedSpoilsV3} />
             </TabPanel>
           </TabPanels>
         </Tabs>
